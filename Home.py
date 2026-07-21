fe3057d32841835a125e1e21de7d53f90bda6efa
Hana Karim <nona.karim25@gmail.com>
2026-07-21 23:48:16 +0300

    Add Azkar Page

--- Home.py
+++ Home.py
@@ -10,7 +10,7 @@ col1, col2, col3 = st.columns([1,3,1])
 # Home
 if st.session_state.page == "Home":
     with col2:
-        st.title("NoNa Islam")
+        st.title("YoNo Islam")
 
     labels = ["Azkar", "Quraan", "Tasbih", "Treasure Hunt"]
     cols = st.columns(len(labels))
@@ -29,4 +29,35 @@ if st.session_state.page == "Home":
             if st.button(label):
                 st.session_state.page = label
                 st.rerun()
+
+# Azkar
+elif st.session_state.page == "Azkar":
+    with col2:
+        st.title("Azkar")
+
+    if st.button("⬅ Home"):
+        st.divider()
+        st.session_state.page = "Home"
+        st.rerun()
+    
+    else:
+        st.divider()
+        labels = ["Morning Azkar", "Evening Azkar"]
+        cols = st.columns(len(labels))
+
+        
+
+        for col, label in zip(cols, labels):
+            with col:
+                if label == "Morning Azkar":
+                    st.image("images/A.jpg", width=80)
+                elif label == "Evening Azkar":
+                    st.image("images/A.jpg", width=80)
+                    
+                if st.button(label):
+                    if label == "Morning Azkar":
+                        st.image("images/MA.png")
+                    else:
+                        st.image("images/EA.png")
+
 
